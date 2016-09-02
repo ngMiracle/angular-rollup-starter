2c5eb75ee4264762865267de681bb2ebdf3f751a
Trotyl <trotyl@qq.com>
2016-09-02 17:34:18 +0800

    Add build script.

--- config/webpack.debug.js
+++ config/webpack.debug.js
@@ -12,7 +12,7 @@ module.exports = {
     },
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, '../dist'),
         filename: '[name].bundle.js',
         sourceMapFilename: '[name].map',
         chunkFilename: '[id].chunk.js',
