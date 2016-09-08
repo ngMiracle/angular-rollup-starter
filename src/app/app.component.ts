8c71c784e81f0a6e8e54badfb47b9f0236f6f3f4
Trotyl <trotyl@qq.com>
2016-09-08 23:00:55 +0800

    Change title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import {Component} from '@angular/core'
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    date: Date = new Date()
-}
+export class AppComponent { }
