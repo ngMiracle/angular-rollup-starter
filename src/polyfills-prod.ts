2e8a605fe3f86ddc410071f6568cd57dec232f9b
Trotyl <trotyl@qq.com>
2016-09-08 14:53:05 +0800

    Add log for environment.

--- src/polyfills-prod.ts
+++ src/polyfills-prod.ts
@@ -1,7 +1,5 @@
 import 'core-js/fn/reflect/define-metadata'
 import 'core-js/fn/reflect/get-metadata'
 import 'core-js/fn/reflect/get-own-metadata'
-//
-// import 'rxjs/Observable'
-//
+
 import 'zone.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const env = process.env.NODE_ENV || 'local'
 
+console.log(`Start webpack in region ${env}`)
+
 switch (env) {
     case 'local':
         module.exports = require('./config/webpack.debug')
