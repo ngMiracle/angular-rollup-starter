a2c8c19a1f796b77e0821d993eb4e38b3879b476
Trotyl <trotyl@qq.com>
2016-09-08 23:47:41 +0800

    Update explanation in main-prod.

--- src/main-prod.ts
+++ src/main-prod.ts
@@ -1,4 +1,4 @@
-// Please comment this file in the first compilation
+// Please comment this file in the first compilation since the .ngfactory.ts files are not generated yet.
 
 import { enableProdMode } from '@angular/core'
 import { platformBrowser } from '@angular/platform-browser'
